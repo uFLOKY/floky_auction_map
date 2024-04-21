853a0110163d5c9f238d7aa0a131f83a66e2c30d
FLOKY <80961359+uFLOKY@users.noreply.github.com>
2024-04-21 19:00:01 +0300

    Add files via upload

---
+++ floky_auction/fxmanifest.lua
@@ -0,0 +1,16 @@
+lua54 'yes'
+fx_version "cerulean"
+games { 'gta5' }
+
+
+this_is_a_map 'yes'
+
+
+files {
+    'floky_timecycle.xml',
+    'floky_auction_doors_game.dat151.rel',
+}
+
+data_file 'TIMECYCLEMOD_FILE' 'floky_timecycle.xml'
+data_file 'AUDIO_GAMEDATA' 'floky_auction_doors_game.dat'
+
